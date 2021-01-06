062a9570318f62a554279819bd90a51d746a1e4c
David <dmaster18@gmail.com>
2021-01-05 19:49:59 -0500

    Update Leaderboard.js

---
+++ frontend/src/Leaderboard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+function mapDispatchToProps(dispatch){
+  return {}
+}
+
+function mapStateToProps(state) {
+  return {}
+}
+
+class Leaderboard extends Component {
+
+
+
+
+  render() {
+
+  }
+
+
+
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Leaderboard)
