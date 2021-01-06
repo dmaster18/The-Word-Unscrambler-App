3d8a859c0a4a853d9a97b57dba0bab2178ae839c
David <dmaster18@gmail.com>
2021-01-05 20:16:35 -0500

    Updated Leaderboard Component

--- frontend/src/Leaderboard.js
+++ frontend/src/Leaderboard.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-
-function mapDispatchToProps(dispatch){
-  return {}
-}
-
-function mapStateToProps(state) {
-  return {}
-}
-
-class Leaderboard extends Component {
-
-
-
-
-  render() {
-
+export default class Leaderboard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playerData: []
+    }
   }
 
+  fetchPlayers () {
+    const playersURL = 'http://127.0.0.1:3000/players';
+    const playerData = fetch(playersURL).then(resp => resp.json());
+    return playerData;
+  }
 
+  renderLeaderboardData() {
+    const sortedPlayerData = this.state.playerData.sort(function (a, b) { return b.attributes.score - a.attributes.score });
+    const leaderboardData = sortedPlayerData.map(player => <tr><td>{player.attributes.name}</td> <td>{player.attributes.score} Points</td></tr>);
+    return leaderboardData;
+  }
 
+  componentDidMount() {
+    this.fetchPlayers().then(json => {
+    this.setState(
+      {playerData: json.data,
+      }
+    )})
+  }
+
+  render () {
+    return (
+      <div>
+        <table id='leaderboard'>
+          <tr><th>Name</th><th>Score</th></tr>
+          {this.renderLeaderboardData()}
+        </table>
+      </div>
+    )
+  }
 
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(Leaderboard)
