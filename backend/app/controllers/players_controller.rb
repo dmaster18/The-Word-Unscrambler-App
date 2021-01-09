7dc786f2304ed311a5de16fe19d2a357f873008b
David <dmaster18@gmail.com>
2021-01-09 13:53:31 -0500

    Added Create Route to PlayersController

--- backend/app/controllers/players_controller.rb
+++ backend/app/controllers/players_controller.rb
@@ -1,10 +1,16 @@
 class PlayersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     players = Player.all
     options = {
       include: []
     }
     render json: PlayerSerializer.new(players, options)
+  end
+
+  def create
+    Player.create(name: params[:player][:name], score: params[:player][:score])
   end
 
 end
